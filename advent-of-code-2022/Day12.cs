0b75d98c5f1ad2fcf9538b03b12ff79f43f2da77
Doug <8922663+douggal@users.noreply.github.com>
2022-12-13 23:15:20 -0500

    Pt 1 works with test data

--- advent-of-code-2022/Day12.cs
+++ advent-of-code-2022/Day12.cs
@@ -74,10 +74,11 @@ namespace advent_of_code_2022
             // because it's easier to find the edges of each vertex that way
             var nCols = input.First().Length;
             var nRows = input.Count;
+
+            // letters in order of height a = lowest and z = highest
+            // rather than rely on comparing ASCII or Unicode points
+            // comparisions will reference ordering in this list
             var letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray()
-                .Select(x => x.ToString())
-                .ToList();
-            var lettersNext = "bcdefghijklmnopqrstuvwxyzz".ToCharArray()
                 .Select(x => x.ToString())
                 .ToList();
 
@@ -102,58 +103,63 @@ namespace advent_of_code_2022
             Dictionary<int, string> labels = new();
             var startVertexID = -1;
             var endVertexID = -1;
+
+            // ID of each vertex in the graph numbered from
+            // top left to bottom right
+            var id = 0;
+
             for (int i = 0; i < nRows; i++)
             {
                 for (int j = 0; j < nCols; j++)
                 {
-                    var id = (i + 1) * (j + 1);
-                    var thisHeight = temp[i, j];
-                    labels[id] = thisHeight;
-                    if (string.Compare(thisHeight, "S") == 0)
+                    id++;
+                    heightmap[id] = new List<int>();
+
+                    var here = temp[i, j];
+                    labels[id] = here;
+                    if (string.Compare(here, "S") == 0)
                     {
                         startVertexID = id;
-                        thisHeight = "a";  // start equiv a
+                        here = "a";  // start equiv a
                     }
-                    if (string.Compare(thisHeight, "E") == 0)
+                    if (string.Compare(here, "E") == 0)
                     {
                         endVertexID = id;
-                        thisHeight = "z";  // end equiv z
+                        here = "z";  // end equiv z
                     }
-                    if (!heightmap.ContainsKey(id))
-                        heightmap[id] = new List<int>();
 
                     // right
-                    if (j < nCols - 2)
+                    if (j < nCols - 1)
                     {
-                        var right = temp[i, j + 1];
+                        var right = CheckLetter(temp[i, j + 1]);
                         var rightVertexID = id + 1;
 
                         // if right equals current or
                         // or it is any amount lower
-                        // or right is no more than 1 char higher
-                        var idx = letters.IndexOf(thisHeight);
-                        var next = lettersNext[idx];
-                        if (String.Compare(right, thisHeight) <= 0)
+                        // or right is no more than 1 char next in order/higher
+                        var idx = letters.IndexOf(here);
+                        var next = letters.IndexOf(right);
+                        if (String.Compare(right, here) <= 0)
                             heightmap[id].Add(rightVertexID);
-                        else if (letters.IndexOf(next) - idx == 1)
+                        else if (next - idx == 1)
                             heightmap[id].Add(rightVertexID);
                         else;   // do nothing
                     }
 
                     // down
-                    if (i < nRows - 2)
+                    if (i < nRows - 1)
                     {
-                        var down = temp[i + 1, j];
+                        var down = CheckLetter(temp[i + 1, j]);
                         var downVertexID = id + nCols;
 
                         // if right equals current or
                         // or it is any amount lower
                         // or right is no more than 1 char higher
-                        var idx = letters.IndexOf(thisHeight);
-                        var next = lettersNext[idx];
-                        if (String.Compare(down, thisHeight) <= 0)
+                        var idx = letters.IndexOf(here);
+                        var next = letters.IndexOf(down);
+                        if (String.Compare(down, here) <= 0)
                             heightmap[id].Add(downVertexID);
-                        else if (letters.IndexOf(next) - idx == 1)
+                        else if (next - idx == 1)
                             heightmap[id].Add(downVertexID);
                         else;   // do nothing
                     }
@@ -161,17 +167,17 @@ namespace advent_of_code_2022
                     // left
                     if (j > 0)
                     {
-                        var left = temp[i, j-1];
+                        var left = CheckLetter(temp[i, j - 1]);
                         var leftVertexID = id - 1;
 
                         // if right equals current or
                         // or it is any amount lower
                         // or right is no more than 1 char higher
-                        var idx = letters.IndexOf(thisHeight);
-                        var next = lettersNext[idx];
-                        if (String.Compare(left, thisHeight) <= 0)
+                        var idx = letters.IndexOf(here);
+                        var next = letters.IndexOf(left);
+                        if (String.Compare(left, here) <= 0)
                             heightmap[id].Add(leftVertexID);
-                        else if (letters.IndexOf(next) - idx == 1)
+                        else if (next - idx == 1)
                             heightmap[id].Add(leftVertexID);
                         else;   // do nothing
                     }
@@ -179,25 +185,32 @@ namespace advent_of_code_2022
                     // up
                     if (i > 0)
                     {
-                        var up = temp[i-1, j];
+                        var up = CheckLetter(temp[i - 1, j]);
                         var upVertexID = id - nCols;
 
                         // if right equals current or
                         // or it is any amount lower
                         // or right is no more than 1 char higher
-                        var idx = letters.IndexOf(thisHeight);
-                        var next = lettersNext[idx];
-                        if (String.Compare(up, thisHeight) <= 0)
+                        var idx = letters.IndexOf(here);
+                        var next = letters.IndexOf(up);
+                        if (String.Compare(up, here) <= 0)
                             heightmap[id].Add(upVertexID);
-                        else if (letters.IndexOf(next) - idx == 1)
+                        else if (next - idx == 1)
                             heightmap[id].Add(upVertexID);
                         else;   // do nothing
                     }
                 }
             }
 
-            //  workaround: could not find a "ConvertAll" for List
-            // convert Dictionary's values from List<int> to int[]
+            // print tree
+            foreach (var kv in heightmap.OrderBy(k => k.Key))
+            {
+                Console.WriteLine($"{kv.Key}: {String.Join(',', kv.Value)}");
+            }
+
+
+            // workaround: could not find a "ConvertAll" for List<T>
+            // copy to new Dictionary but turn the values from List<int> to int[]
             Dictionary<int, int[]> heightmap2 = new();
             foreach (var kv in heightmap)
             {
@@ -235,9 +248,10 @@ namespace advent_of_code_2022
                 }
             }
 
-            var xyz = 0;
+            var answer = 0;
 
             Console.WriteLine("Day 12 Part 1");
+            Console.WriteLine($"{answer}\n\n");
 
 
             // Part Two
@@ -250,6 +264,15 @@ namespace advent_of_code_2022
             Console.WriteLine("Time elapsed: {0:0.0} ms", stopwatch.ElapsedMilliseconds);
             Console.WriteLine($"End timestamp {DateTime.UtcNow.ToString("O")}");
             //Console.ReadKey();
+        }
+
+        private static string CheckLetter(string letter)
+        {
+            var ret = letter;
+            if (string.Compare(letter, "S") == 0) ret = "a";
+            if (string.Compare(letter, "E") == 0) ret = "z";
+
+            return ret;
         }
     }
 }
